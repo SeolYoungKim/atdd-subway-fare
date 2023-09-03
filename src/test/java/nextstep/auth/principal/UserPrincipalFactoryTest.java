7d8893e767f00a9e1b7dfb298b4f1539e0f25f4b
SeolYoungKim <nasur4da@gmail.com>
2023-09-03 17:18:16 +0900

    test: 로그인 유저의 UserPrincipal 생성

--- src/test/java/nextstep/auth/principal/UserPrincipalFactoryTest.java
+++ src/test/java/nextstep/auth/principal/UserPrincipalFactoryTest.java
@@ -1,11 +1,16 @@
 package nextstep.auth.principal;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("UserPrincipalFactory 단위 테스트")
 class UserPrincipalFactoryTest {
+    private static final String AUTHORIZATION_HEADER = "bearer token";
+
     private UserPrincipalFactory userPrincipalFactory;
 
     @BeforeEach
@@ -14,9 +19,16 @@ class UserPrincipalFactoryTest {
     }
 
     @DisplayName("로그인 유저의 UserPrincipal 생성")
-    @Test
-    void createLoggedInUser() {
+    @ParameterizedTest(name = "isLoginRequired={0}")
+    @ValueSource(booleans = {true, false})
+    void createLoggedInUser(boolean isLoginRequired) {
         // given
+        AuthorizationHeader authorizationHeader = new AuthorizationHeader(AUTHORIZATION_HEADER);
 
+        // when
+        UserPrincipal userPrincipal = userPrincipalFactory.create(authorizationHeader, isLoginRequired);
+
+        // then
+        assertThat(userPrincipal).isInstanceOf(LoggedInUserPrincipal.class);
     }
 }
